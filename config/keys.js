5795bc41eb0392bebb18c2a9a0e43df651afbb7b
Yitong He <yitonghe@usc.edu>
2018-09-08 01:49:12 -0700

    Add register login current route with authentication & validation

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://yitonghe:bohebohe520@ds249942.mlab.com:49942/reactdevconnector"
+    "mongodb://yitonghe:bohebohe520@ds249942.mlab.com:49942/reactdevconnector",
+  secretOrKey: "secret"
 };
