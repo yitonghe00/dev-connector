9e56a624bd3e5bb6bfe4cb2e7ed23cd44d926a9c
Yitong He <yitonghe@usc.edu>
2018-09-09 00:45:45 -0700

    Add get profile by id api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -43,17 +43,41 @@ router.get(
 // @access Public
 router.get("/handle/:handle", (req, res) => {
   const errors = {};
+
   Profile.findOne({ handle: req.params.handle })
     .populate("user", ["name", "avatar"])
     .then(profile => {
       if (!profile) {
         errors.noprofile = "There is no profile for this user.";
         res.status(404).json(errors);
+        return;
       }
 
       res.json(profile);
     })
     .catch(err => res.status(404).json(err));
+});
+
+// @route  GET api/profile/user/:user_id
+// @desc   Get profile by user id
+// @access Public
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user.";
+        res.status(404).json(errors);
+        return;
+      }
+
+      res.json(profile);
+    })
+    .catch(err =>
+      res.status(404).json({ profile: "There is no profile for this user." })
+    );
 });
 
 // @route  POST api/profile
