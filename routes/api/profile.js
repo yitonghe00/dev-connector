062fcb1ee0fb084ab5031299a05eb8c9057f0437
Yitong He <yitonghe@usc.edu>
2018-09-08 20:43:41 -0700

    Add current user Profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -13,4 +13,24 @@ const User = require("../../models/User");
 // @access Public
 router.get("/test", (req, res) => res.json({ msg: "Profile works" }));
 
+// @route  GET api/profile
+// @desc   Get current user profile
+// @access Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user.";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
 module.exports = router;
