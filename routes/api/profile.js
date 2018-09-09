e22f810b71cf5092ced11fbd2f64924b5aea0d1e
Yitong He <yitonghe@usc.edu>
2018-09-09 00:29:32 -0700

    Add get profile by handle api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -38,6 +38,24 @@ router.get(
   }
 );
 
+// @route  GET api/profile/handle/:handle
+// @desc   Get profile by handle
+// @access Public
+router.get("/handle/:handle", (req, res) => {
+  const errors = {};
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user.";
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 // @route  POST api/profile
 // @desc   Create or edit user profile
 // @access Private
